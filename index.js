2a61e2615671e2e912e77cc41150053769a1c8a3
Waduzzaman <waduzzaman@gmail.com>
2024-05-13 07:36:22 -0400

    borrowed book functions added

--- index.js
+++ index.js
@@ -1,132 +1,149 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require( 'express' );
+const cors = require( 'cors' );
+require( 'dotenv' ).config();
+const { MongoClient, ServerApiVersion, ObjectId } = require( 'mongodb' );
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use( cors() );
+app.use( express.json() );
 
 // MongoDB connection
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nrvepld.mongodb.net`;
+const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.nrvepld.mongodb.net`;
 
 
-async function run() {
+async function run ()
+{
 
-    const client = new MongoClient(uri, {
+    const client = new MongoClient( uri, {
         serverApi: {
-          version: ServerApiVersion.v1,
-          strict: true,
-          deprecationErrors: true,
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
         }
-      });
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    } );
+    try
+    {
+        // Connect the client to the server (optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db( "admin" ).command( { ping: 1 } );
 
-    // Declare Books Collection
-    const booksCollection = client.db( 'booksDB' ).collection( 'books' );
-    
+        // Declare Books Collection
+        const booksCollection = client.db( 'booksDB' ).collection( 'books' );
 
-    app.get( '/books', async ( req, res ) =>
+
+        app.get( '/books', async ( req, res ) =>
         {
-          const cursor = booksCollection.find();
-          const result = await cursor.toArray();
-          res.send( result );
+            const cursor = booksCollection.find();
+            const result = await cursor.toArray();
+            res.send( result );
         } );
 
-          // Post / add a book in the database 
-    app.post( '/books', async ( req, res ) =>
+        // Post / add a book in the database 
+        app.post( '/books', async ( req, res ) =>
         {
-          const newBooks = req.body;
-          console.log( newBooks );
-          const result = await booksCollection.insertOne( newBooks );
-          res.send( result );
+            const newBooks = req.body;
+            console.log( newBooks );
+            const result = await booksCollection.insertOne( newBooks );
+            res.send( result );
         } )
 
-          // find one book by id and return it as json
-    app.get( '/books/:id', async ( req, res ) =>
+        // find one book by id and return it as json
+        app.get( '/books/:id', async ( req, res ) =>
         {
-          const id = req.params.id;
-          const query = { _id: new ObjectId( id ) }
-          const result = await booksCollection.findOne( query );
-          res.send( result );
+            const id = req.params.id;
+            const query = { _id: new ObjectId( id ) }
+            const result = await booksCollection.findOne( query );
+            console.log( 'one book found', result );
+            res.send( result );
         } )
 
 
-        
-        // find books with category name
+        // Borrowed book query: 
 
-        // app.get("/books/:category", (req, res) => {
-        //     const category = req.params.category; // Extract category from route params
-          
-        //     // Filter books based on category if category is provided
-        //     let filteredBooks = books;
-        //     if (category) {
-        //       filteredBooks = books.filter((book) => book.category === category);
-        //     }
-          
-        //     res.json(filteredBooks);
-        //   });
+        app.post( '/books/:id/borrow', async ( req, res ) =>
+        {
+            try
+            {
+                const bookId = req.params.id;
+                const { returnDate, userEmail, userName } = req.body;
 
-        // Declare Program Collection 
-    const programsCollection = client.db( 'programsDB' ).collection( 'programs' );
+                // Update book quantity using $inc operator
+                await Book.findByIdAndUpdate( bookId, { $inc: { quantity: -1 } } );
 
-   // Collection for programs
+                // Add book to Borrowed Books collection or update existing record
+                await BorrowedBook.findOneAndUpdate(
+                    { bookId, userEmail },
+                    { returnDate, userName },
+                    { upsert: true }
+                );
 
-
-   // Get all programs
-   app.get('/programs', async (req, res) => {
-       try {
-           const cursor = programsCollection.find();
-           const result = await cursor.toArray();
-           res.json(result);
-       } catch (error) {
-           console.error('Error fetching programs:', error);
-           res.status(500).json({ error: 'Internal server error' });
-       }
-   });
-
-   // Add a new program
-   app.post('/programs', async (req, res) => {
-       try {
-           const newProgram = req.body;
-           const result = await programsCollection.insertOne(newProgram);
-           res.json(result.ops[0]); // Return the inserted document
-       } catch (error) {
-           console.error('Error adding program:', error);
-           res.status(500).json({ error: 'Internal server error' });
-       }
-   });
-
-   // Get a program by ID
-   app.get('/programs/:id', async (req, res) => {
-       try {
-           const id = req.params.id;
-           const query = { _id: new ObjectId(id) };
-           const result = await programsCollection.findOne(query);
-           if (!result) {
-               res.status(404).json({ error: 'Program not found' });
-               return;
-           }
-           res.json(result);
-       } catch (error) {
-           console.error('Error fetching program by ID:', error);
-           res.status(500).json({ error: 'Internal server error' });
-       }
-   });
-    
-
-
-   
+                res.status( 200 ).send( 'Book borrowed successfully' );
+            } catch ( error )
+            {
+                console.error( 'Error borrowing book:', error );
+                res.status( 500 ).send( 'Internal server error' );
+            }
+        } );
 
 
 
+        // Collection for programs
+        const programsCollection = client.db( 'programsDB' ).collection( 'programs' );
+
+        // Get all programs
+        app.get( '/programs', async ( req, res ) =>
+        {
+            try
+            {
+                const cursor = programsCollection.find();
+                const result = await cursor.toArray();
+                res.json( result );
+            } catch ( error )
+            {
+                console.error( 'Error fetching programs:', error );
+                res.status( 500 ).json( { error: 'Internal server error' } );
+            }
+        } );
+
+        // Add a new program
+        app.post( '/programs', async ( req, res ) =>
+        {
+            try
+            {
+                const newProgram = req.body;
+                const result = await programsCollection.insertOne( newProgram );
+                res.json( result.ops[ 0 ] ); // Return the inserted document
+            } catch ( error )
+            {
+                console.error( 'Error adding program:', error );
+                res.status( 500 ).json( { error: 'Internal server error' } );
+            }
+        } );
+
+        // Get a program by ID
+        app.get( '/programs/:id', async ( req, res ) =>
+        {
+            try
+            {
+                const id = req.params.id;
+                const query = { _id: new ObjectId( id ) };
+                const result = await programsCollection.findOne( query );
+                if ( !result )
+                {
+                    res.status( 404 ).json( { error: 'Program not found' } );
+                    return;
+                }
+                res.json( result );
+            } catch ( error )
+            {
+                console.error( 'Error fetching program by ID:', error );
+                res.status( 500 ).json( { error: 'Internal server error' } );
+            }
+        } );
 
 
 
@@ -135,16 +152,26 @@ async function run() {
 
 
 
-    // Start the server after setting up routes
-    app.listen(port, () => {
-      console.log(`Community Library server is running on port: ${port}`);
-    });
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-  } finally {
-    // Ensure that the client will close when you finish/error
-    // await client.close();
-  }
+
+
+
+
+
+
+
+        // Start the server after setting up routes
+        app.listen( port, () =>
+        {
+            console.log( `Community Library server is running on port: ${ port }` );
+        } );
+    } catch ( err )
+    {
+        console.error( "Error connecting to MongoDB:", err );
+    } finally
+    {
+        // Ensure that the client will close when you finish/error
+        // await client.close();
+    }
 }
 
-run().catch(console.dir);
+run().catch( console.dir );
